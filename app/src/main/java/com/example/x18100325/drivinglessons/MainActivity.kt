5caa95de91abdb2114f401a20a13f1b421334520
x18100325 <genazzano84@gmail.com>
2019-02-22 16:02:56 +0000

    initial rep

---
+++ app/src/main/java/com/example/x18100325/drivinglessons/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.x18100325.drivinglessons
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
